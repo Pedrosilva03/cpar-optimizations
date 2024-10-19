f75864544833ce37a90ad6d1a3804981dce9f331
dbarr0s <diogo.brafael@gmail.com>
2024-10-19 23:27:50 +0100

    Alterações feitas

--- trabalho1/project/fluid_solver.cpp
+++ trabalho1/project/fluid_solver.cpp
@@ -26,31 +26,71 @@ void set_bnd(int M, int N, int O, int b, float *x) {
   // Set boundary on faces
   for (i = 1; i <= M; i++) {
     for (j = 1; j <= N; j++) {
-      x[IX(i, j, 0)] = b == 3 ? -x[IX(i, j, 1)] : x[IX(i, j, 1)];
-      x[IX(i, j, O + 1)] = b == 3 ? -x[IX(i, j, O)] : x[IX(i, j, O)];
+      // CHANGES BEGIN //
+      int IX1 = IX(i, j, 0);
+      int IX2 = IX(i, j, O + 1);
+      int IX3 = IX(i, j, 1);
+      int IX4 = IX(i, j, O);
+
+      x[IX1] = b == 3 ? -x[IX3] : x[IX3];
+      x[IX2] = b == 3 ? -x[IX4] : x[IX4];
+      // CHANGES END //
     }
   }
   for (i = 1; i <= N; i++) {
     for (j = 1; j <= O; j++) {
-      x[IX(0, i, j)] = b == 1 ? -x[IX(1, i, j)] : x[IX(1, i, j)];
-      x[IX(M + 1, i, j)] = b == 1 ? -x[IX(M, i, j)] : x[IX(M, i, j)];
+      // CHANGES BEGIN //
+      int IX1 = IX(0, i, j);
+      int IX2 = IX(M + 1, i, j);
+      int IX3 = IX(1, i, j);
+      int IX4 = IX(M, i, j);
+
+      x[IX1] = b == 1 ? -x[IX3] : x[IX3];
+      x[IX2] = b == 1 ? -x[IX4] : x[IX4];
+      // CHANGES END //
     }
   }
   for (i = 1; i <= M; i++) {
     for (j = 1; j <= O; j++) {
-      x[IX(i, 0, j)] = b == 2 ? -x[IX(i, 1, j)] : x[IX(i, 1, j)];
-      x[IX(i, N + 1, j)] = b == 2 ? -x[IX(i, N, j)] : x[IX(i, N, j)];
+      // CHANGES BEGIN //
+      int IX1 = IX(i, 0, j);
+      int IX2 = IX(i, N + 1, j);
+      int IX3 = IX(i, 1, j);
+      int IX4 = IX(i, N, j);
+
+      x[IX1] = b == 2 ? -x[IX3] : x[IX3];
+      x[IX2] = b == 2 ? -x[IX4] : x[IX4];
+      // CHANGES END //
     }
   }
 
+  //CHANGES BEGIN//
+  int IX1 = IX(0, 0, 0);
+  int IX2 = IX(M + 1, 0, 0);
+  int IX3 = IX(0, N + 1, 0);
+  int IX4 = IX(M + 1, N + 1, 0);
+
+  int IX5 = IX(1, 0, 0);
+  int IX6 = IX(0, 1, 0); 
+  int IX7 = IX(0, 0, 1);
+
+  int IX9 = IX(M, 0, 0);
+  int IX10 = IX(M + 1, 1, 0);
+  int IX11 = IX(M + 1, 0, 1);
+
+  int IX13 = IX(1, N + 1, 0);
+  int IX14 = IX(0, N, 0);
+  int IX15 = IX(0, N + 1, 1);
+
+  int IX17 = IX(M, N + 1, 0);
+  int IX18 = IX(M + 1, N, 0);
+  int IX19 = IX(M + 1, N + 1, 1);
+
   // Set corners
-  x[IX(0, 0, 0)] = 0.33f * (x[IX(1, 0, 0)] + x[IX(0, 1, 0)] + x[IX(0, 0, 1)]);
-  x[IX(M + 1, 0, 0)] =
-      0.33f * (x[IX(M, 0, 0)] + x[IX(M + 1, 1, 0)] + x[IX(M + 1, 0, 1)]);
-  x[IX(0, N + 1, 0)] =
-      0.33f * (x[IX(1, N + 1, 0)] + x[IX(0, N, 0)] + x[IX(0, N + 1, 1)]);
-  x[IX(M + 1, N + 1, 0)] = 0.33f * (x[IX(M, N + 1, 0)] + x[IX(M + 1, N, 0)] +
-                                    x[IX(M + 1, N + 1, 1)]);
+  x[IX1] = 0.33f * (x[IX5] + x[IX6] + x[IX7]);
+  x[IX2] = 0.33f * (x[IX9] + x[IX10] + x[IX11]);
+  x[IX3] = 0.33f * (x[IX13] + x[IX14] + x[IX15]);
+  x[IX4] = 0.33f * (x[IX17] + x[IX18] + x[IX19]);
 }
 
 // Linear solve for implicit methods (diffusion)
@@ -117,23 +157,30 @@ void advect(int M, int N, int O, int b, float *d, float *d0, float *u, float *v,
   for (int i = 1; i <= M; i++) {
     for (int j = 1; j <= N; j++) {
       for (int k = 1; k <= O; k++) {
-        float x = i - dtX * u[IX(i, j, k)];
-        float y = j - dtY * v[IX(i, j, k)];
-        float z = k - dtZ * w[IX(i, j, k)];
+        // CHANGES BEGIN //
+        int IX1 = IX(i, j, k);
+
+        float x = i - (dtX * u[IX1]);
+        float y = j - (dtY * v[IX1]);
+        float z = k - (dtZ * w[IX1]);
 
         // Clamp to grid boundaries
+        float if1 = M + 0.5f;
+        float if2 = N + 0.5f;
+        float if3 = O + 0.5f;
+
         if (x < 0.5f)
           x = 0.5f;
-        if (x > M + 0.5f)
-          x = M + 0.5f;
+        if (x > if1)
+          x = if1;
         if (y < 0.5f)
           y = 0.5f;
-        if (y > N + 0.5f)
-          y = N + 0.5f;
+        if (y > if2)
+          y = if2;
         if (z < 0.5f)
           z = 0.5f;
-        if (z > O + 0.5f)
-          z = O + 0.5f;
+        if (z > if3)
+          z = if3;
 
         int i0 = (int)x, i1 = i0 + 1;
         int j0 = (int)y, j1 = j0 + 1;
@@ -143,19 +190,27 @@ void advect(int M, int N, int O, int b, float *d, float *d0, float *u, float *v,
         float t1 = y - j0, t0 = 1 - t1;
         float u1 = z - k0, u0 = 1 - u1;
 
-        d[IX(i, j, k)] =
-            s0 * (t0 * (u0 * d0[IX(i0, j0, k0)] + u1 * d0[IX(i0, j0, k1)]) +
-                  t1 * (u0 * d0[IX(i0, j1, k0)] + u1 * d0[IX(i0, j1, k1)])) +
-            s1 * (t0 * (u0 * d0[IX(i1, j0, k0)] + u1 * d0[IX(i1, j0, k1)]) +
-                  t1 * (u0 * d0[IX(i1, j1, k0)] + u1 * d0[IX(i1, j1, k1)]));
+        int IX2 = IX(i0, j0, k0);
+        int IX3 = IX(i0, j0, k1);
+        int IX4 = IX(i0, j1, k0);
+        int IX5 = IX(i0, j1, k1);
+        int IX6 = IX(i1, j0, k0);
+        int IX7 = IX(i1, j0, k1);
+        int IX8 = IX(i1, j1, k0);
+        int IX9 = IX(i1, j1, k1);
+
+        d[IX1] =
+            s0 * (t0 * (u0 * d0[IX2] + u1 * d0[IX3]) +
+                  t1 * (u0 * d0[IX4] + u1 * d0[IX5])) +
+            s1 * (t0 * (u0 * d0[IX6] + u1 * d0[IX7]) +
+                  t1 * (u0 * d0[IX8] + u1 * d0[IX9]));
+        // CHANGES END //
       }
     }
   }
   set_bnd(M, N, O, b, d);
 }
 
-// Projection step to ensure incompressibility (make the velocity field
-// divergence-free)
 void project(int M, int N, int O, float *u, float *v, float *w, float *p, float *div) {
     // Calculate the divisor once to avoid recalculating
     float maxSize = MAX(M, MAX(N, O));
@@ -203,9 +258,18 @@ void project(int M, int N, int O, float *u, float *v, float *w, float *p, float 
         for (int j = 1; j <= N; j++) {
             for (int i = 1; i <= M; i++) {
                 int idx = IX(i, j, k);
-                u[idx] -= 0.5f * (p[IX(i + 1, j, k)] - p[IX(i - 1, j, k)]);
-                v[idx] -= 0.5f * (p[IX(i, j + 1, k)] - p[IX(i, j - 1, k)]);
-                w[idx] -= 0.5f * (p[IX(i, j, k + 1)] - p[IX(i, j, k - 1)]);
+                // CHANGES BEGIN //
+                int IX1 = IX(i + 1, j, k);
+                int IX2 = IX(i - 1, j, k);
+                int IX3 = IX(i, j + 1, k);
+                int IX4 = IX(i, j - 1, k);
+                int IX5 = IX(i, j, k + 1);
+                int IX6 = IX(i, j, k - 1);
+
+                u[idx] -= 0.5f * (p[IX1] - p[IX2]);
+                v[idx] -= 0.5f * (p[IX3] - p[IX4]);
+                w[idx] -= 0.5f * (p[IX5] - p[IX6]);
+                // CHANGES END //
             }
         }
     }
@@ -215,8 +279,6 @@ void project(int M, int N, int O, float *u, float *v, float *w, float *p, float 
     set_bnd(M, N, O, 2, v);
     set_bnd(M, N, O, 3, w);
 }
-
-
 
 // Step function for density
 void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v,
