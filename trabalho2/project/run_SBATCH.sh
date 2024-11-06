6b9d8881a3244c0f770eb1e3ba8e67149d5d9805
anightatheopera <ritalino7@hotmail.com>
2024-11-06 17:02:44 +0000

    debugging

--- trabalho2/project/run_SBATCH.sh
+++ trabalho2/project/run_SBATCH.sh
@@ -12,7 +12,8 @@ module load gcc/11.2.0
 # Set OMP_NUM_THREADS to the number of CPUs per task allocated by Slurm
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-# Run the application with `perf` for performance metrics
-srun perf stat -r 3 -M cpi,instructions \
+# run app
+srun --partition=cpar --exclusive \
+     perf stat -r 3 -M instructions \
      -e branch-misses,L1-dcache-loads,L1-dcache-load-misses,cycles,duration_time,mem-loads,mem-stores \
      ./fluid_sim
